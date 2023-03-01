c0af7723f003a72927a4ecc3705d4c3e7eabcad1
Common Vulnerabilities and Exploits Database <84289402+gitadvisor@users.noreply.github.com>
2023-03-01 08:26:33 +0600

    Update goscan.go
    Signed-off-by: Common Vulnerabilities and Exploits Database <84289402+gitadvisor@users.noreply.github.com>

--- goscan.go
+++ goscan.go
@@ -1,1 +1,19 @@
+package main
 
+import(
+      "bufio"
+      "crypto/tls"
+      "encoding/json"
+      "errors"
+      "flag"
+      "fmt"
+      "log"
+      "net/url"
+      "os"
+      "regexe"
+      "strings"
+      "sync"
+      "time"
+  
+      "github.com/gocally/colly/v2"
+)
